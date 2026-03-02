b09a96f35da3f1ab668eac40d85fcae19f8cb454
Md Rahad <rahad3100@gmail.com>
2026-03-02 22:33:39 +0600

    updated

--- InventoryManagement.Domain/Entities/Product.cs
+++ InventoryManagement.Domain/Entities/Product.cs
@@ -9,6 +9,6 @@ namespace InventoryManagement.Domain.Entities
         public int Quantity { get; set; }
         public decimal Price { get; set; }
 
-        //public ICollection<StockTransaction>? StockTransactions { get; set; }
+        
     }
 }
